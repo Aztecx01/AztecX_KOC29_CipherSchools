fc87d828fc53f4485770cbf8fd83340cec81e449
Rishav <rishavraj4273@gmail.com>
2022-11-20 14:02:16 +0530

    added command for non leap year

--- python_proj.py
+++ python_proj.py
@@ -1,7 +1,7 @@
 print("Program to find leap year")
 
-a = (input("Enter initial date(in ddmmyyyy format): "))
-b = (input("Enter final date(in ddmmyyyy format): "))
+a = (input("Enter initial date(in dd/mm/yyyy format): "))
+b = (input("Enter final date(in dd/mm/yyyy format): "))
 
 y1 = int(a[-4:])
 y2 = int(b[-4:])
@@ -10,14 +10,22 @@ def leap(y1,y2):
     print("\nAll leap years between {} to {} are".format(y1,y2))
     for x in range(y1,y2):
         if( x%400==0):
-            print(x)
+            print(x,end=(", "))
         elif(x%100!=0 and x%4==0):
-            print(x)
-
+            print(x,end=(", "))
+def notleap(y1,y2):
+    print("\nAll non leap years between {} to {} are:".format(y1,y2))
+    for x in range(y1,y2):
+        if( x%100==0 and x%400!=0):
+            print(x,end=(", "))
+        elif(x%100!=0 and x%4!=0):
+            print(x,end=(", "))
 if(y1>y2):
     print("\nFinal date can't be greater than initial date")
     exit()
 else:
     leap(y1,y2)
+    print("")
+    notleap(y1,y2)
 
 
