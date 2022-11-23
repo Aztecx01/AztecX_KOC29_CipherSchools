b03cfcd50e14771612724cf10dc50adb514de3f0
Rishav <rishavraj4273@gmail.com>
2022-11-23 18:27:16 +0530

    minor changes

--- python_proj.py
+++ python_proj.py
@@ -5,27 +5,27 @@ b = (input("Enter final date(in dd/mm/yyyy format): "))
 
 y1 = int(a[-4:])
 y2 = int(b[-4:])
+a = []
+b = []
 
 def leap(y1,y2):
-    print("\nAll leap years between {} to {} are".format(y1,y2))
-    for x in range(y1,y2):
+    for x in range(y1,y2+1):
         if( x%400==0):
-            print(x,end=(", "))
+            a.append(x)
         elif(x%100!=0 and x%4==0):
-            print(x,end=(", "))
-def notleap(y1,y2):
-    print("\nAll non leap years between {} to {} are:".format(y1,y2))
-    for x in range(y1,y2):
-        if( x%100==0 and x%400!=0):
-            print(x,end=(", "))
-        elif(x%100!=0 and x%4!=0):
-            print(x,end=(", "))
+            a.append(x)
+        else:
+            b.append(x)
+    return a,b
+
 if(y1>y2):
     print("\nFinal date can't be greater than initial date")
     exit()
 else:
     leap(y1,y2)
-    print("")
-    notleap(y1,y2)
+    print("\nAll leap years between {} to {} are".format(y1,y2))
+    print(a)
+    print("\nAll non-leap years between {} to {} are".format(y1,y2))
+    print(b)
 
 
